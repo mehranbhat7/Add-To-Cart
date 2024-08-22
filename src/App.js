7cd30062fa2f40fa053d586611084cd9d907f8a6
Mehran <mehranbhat2001@gmail.com>
2024-08-22 16:36:08 +0530

    react add to card day 7

--- src/App.js
+++ src/App.js
@@ -4,13 +4,30 @@ import Shop from "./Components/Shop";
 
 function App() {
   const [cart, setcart] = useState([]);
+  const [warning, setwarning] = useState(false);
   function handleclick(items) {
-    console.log(items);
+    let ispresent = false;
+    cart.forEach((prod) => {
+      if (prod.id === items.id) ispresent = true;
+    });
+    if (ispresent) {
+      setwarning(true);
+      setTimeout(() => {
+        setwarning(false);
+      }, 3000);
+      return;
+    }
+    setcart([...cart, items]);
   }
   return (
     <div>
       <Navbar size={cart.length} />
       <Shop handleclick={handleclick} />
+      {warning && (
+        <div className="fixed bottom-0 left-0 right-0 bg-red-500 p-4 text-center text-white">
+          Item already in cart!
+        </div>
+      )}
     </div>
   );
 }
