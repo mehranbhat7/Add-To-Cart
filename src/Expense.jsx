3485c4c528e5e1891e64c16455780d8666d52d8b
Mehran <mehranbhat2001@gmail.com>
2024-08-22 11:39:22 +0530

    react expense tracker day 7

--- src/Expense.jsx
+++ src/Expense.jsx
@@ -1,20 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Expense = () => {
+  const [inp, setInp] = useState("");
+  const [amount, setAmount] = useState("");
+  const [expenses, setExpenses] = useState([]);
+
+  function handle() {
+    if (!inp || !amount) {
+      return;
+    }
+    const newExpense = {
+      id: expenses.length + 1,
+      title: inp,
+      amount: amount,
+    };
+    setExpenses([...expenses, newExpense]);
+    setInp("");
+    setAmount("");
+  }
+
+  const deletee = (id) => {
+    setExpenses(expenses.filter((expense) => expense.id !== id));
+  };
+
   return (
     <>
       <div>
+        <h1>Expense Tracker</h1>
         <div>
-          <h1>Expense Tracker</h1>
+          <input
+            type="text"
+            placeholder="Enter expense title"
+            required
+            onChange={(e) => setInp(e.target.value)}
+            value={inp}
+          />
+          <input
+            type="number"
+            placeholder="Enter amount"
+            required
+            onChange={(e) => setAmount(e.target.value)}
+            value={amount}
+          />
+          <button onClick={handle}>Add expense</button>
         </div>
         <div>
-          <input type="text" required />
-          <input type="number" required />
-          <button>Add expense</button>
-        </div>
-        <div>
-          <p>BOOK</p>
-          <p>total</p>
+          <ul>
+            {expenses.map((ele) => (
+              <li key={ele.id}>
+                {ele.title} - ${ele.amount}
+                <button onClick={() => deletee(ele.id)}>DELETE</button>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </>
